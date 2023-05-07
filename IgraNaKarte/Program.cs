22bf371fa3caa37fd32bf13cb23c9227860199f0
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-05-07 21:23:29 +0200

    Add files via upload

---
+++ IgraNaKarte/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgraNaKarte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rezultat = 0;
+            int upis;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Upisite vrijednost karte (1 - 13).");
+                    upis = Convert.ToInt32(Console.ReadLine());
+
+                    if (upis >= 1 && upis <= 13) rezultat = rezultat + upis;
+                    else Console.WriteLine("Krivi upis, pokusajte ponovo.");
+
+                }
+                catch (FormatException ex) { Console.WriteLine(ex.Message); }
+
+            } while (rezultat < 31);
+
+
+            if (rezultat == 31 || rezultat > 31) Console.WriteLine("Pobjedili ste");
+            else Console.WriteLine("Izgubili ste");
+
+
+            Console.ReadKey();
+        }
+    }
+}
